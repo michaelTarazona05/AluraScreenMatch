e08b929fed3381c92c0af9873f6991cecb41d882
Michael Tarazona <michaeltara05@gmail.com>
2024-06-11 22:43:57 -0500

    Interacting with the user

--- src/Lectura.java
+++ src/Lectura.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Lectura {
 
     public static void main(String[] args) {
 
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe el nombre de tu pélicula favorita");
+        String pelicula = teclado.nextLine();
+        System.out.println("Ahora escribe la fecha de lanzamiento");
+        int fechaDeLanzamiento = teclado.nextInt();
+        System.out.println("Por último dinos que nota le das a esta pélicula");
+        double nota = teclado.nextDouble();
 
+        System.out.println(pelicula);
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(nota);
 
     }
 }
